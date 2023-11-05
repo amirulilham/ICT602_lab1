b48ebfaab1380f1ef2fa3e231db455c456ee73de
amirulilham <2022484752@student.uitm.edu.my>
2023-11-05 14:57:33 +0800

    3rd commit

--- app/src/main/java/com/example/lab1_ilham/MainActivity.java
+++ app/src/main/java/com/example/lab1_ilham/MainActivity.java
@@ -46,5 +46,6 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(this, "NAME RESET", Toast.LENGTH_SHORT);
         toast.show();
     }
+    //useless
 
 }
